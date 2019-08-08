f3ec868c6daed14178099a2c6ff1b12ba171077e
lukasz.przytocki <lukasz.przytoci@aptitudesoftware.com>
2019-08-08 18:32:21 +0200

    Added displaying category list.

--- src/main/java/pl/lukasz/demo/HomeController.java
+++ src/main/java/pl/lukasz/demo/HomeController.java
@@ -3,9 +3,12 @@ package pl.lukasz.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -23,23 +26,38 @@ public class HomeController {
 
         List<Recipe> recipes = recipeRepository.findThreeMostPopular();
         model.addAttribute("mostPolular", recipes);
+        model.addAttribute("category", categoryRepository.findAll());
         return "home";
     }
 
 
     @GetMapping("/add")
     public String add(Model model) {
-    Recipe recipe = new Recipe();
-    model.addAttribute("newRecipe", recipe);
-    model.addAttribute("category",categoryRepository.findAll());
+        Recipe recipe = new Recipe();
+        model.addAttribute("newRecipe", recipe);
+        model.addAttribute("category", categoryRepository.findAll());
 
         return "addRecipe";
     }
 
     @PostMapping("/add")
-    public String add(Recipe recipe ) {
+    public String add(Recipe recipe) {
         recipeRepository.save(recipe);
-        return "home";
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/display/{id}")
+    public String display(Model model, @PathVariable Long id) {
+
+        if (recipeRepository.findByCategory_Id(id) != null) {
+            model.addAttribute("displaySelected", recipeRepository.findByCategory_Id(id));
+            return "display";
+
+        } else {
+            return "error";
+        }
+
     }
 
 
--- src/main/java/pl/lukasz/demo/RecipeRepository.java
+++ src/main/java/pl/lukasz/demo/RecipeRepository.java
@@ -10,4 +10,7 @@ public interface RecipeRepository extends JpaRepository<Recipe,Long> {
 
     @Query(value = "select * from Recipe order by LIKE_NUMBER DESC LIMIT 3", nativeQuery = true)
     List<Recipe> findThreeMostPopular();
+
+
+    List<Recipe> findByCategory_Id(Long id);
 }
